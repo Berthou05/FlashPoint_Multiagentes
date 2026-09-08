ca79094a6fd27f03a393b75a1f070a7f9d942ff8
Liz <administrador@MacBook-Air-28.local>
2026-09-08 15:36:13 -0600

    environment

--- Unity/Assets/Scripts/Juego/BoardPositionConverter.cs
+++ Unity/Assets/Scripts/Juego/BoardPositionConverter.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class BoardPositionConverter : MonoBehaviour
 {
+    // Tamaño de cada casilla del tablero en Unity
     public float cellSize = 4f;
 
+    // Convierte las coordenadas X,Y de Mesa
+    // a la posición X,Y,Z de Unity
     public Vector3 ConvertToUnityPosition(int x, int y)
     {
         float posicionX = (x - 1) * cellSize;
